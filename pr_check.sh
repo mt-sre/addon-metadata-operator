b3f65981c850e331efa33c9fb72d81af3e599ece
Mayank Shah <m.shah@redhat.com>
2021-10-25 17:14:38 +0530

    Add Dockerfile.pr_check
    Signed-off-by: Mayank Shah <m.shah@redhat.com>

--- pr_check.sh
+++ pr_check.sh
@@ -4,6 +4,6 @@ set -exvo pipefail -o nounset
 
 IMAGE_TEST=addon-metadata-operator
 
-docker build -t ${IMAGE_TEST} -f Dockerfile.test .
+docker build -t ${IMAGE_TEST} -f Dockerfile.pr_check .
 docker run --rm ${IMAGE_TEST}
 make docker-build
