c2993de6eaf31efe5ec4491ae8b6681c0d9abf08
Yashvardhan Kukreja <ykukreja@redhat.com>
2021-10-25 19:24:01 +0530

    Made some fixes around the validate addon function and validate default channel function
    Signed-off-by: Yashvardhan Kukreja <ykukreja@redhat.com>

--- pkg/validators/validator_addon_label.go
+++ pkg/validators/validator_addon_label.go
@@ -6,8 +6,8 @@ import (
 
 // TODO: preferable, have return type as a (bool, error) instead
 func ValidateAddonLabel(metabundle *utils.MetaBundle) (bool, error) {
-	operatorName, label := metabundle.AddonMeta.OperatorName, metabundle.AddonMeta.Label
-	if label != "api.openshift.com/addon-"+operatorName {
+	operatorId, label := metabundle.AddonMeta.ID, metabundle.AddonMeta.Label
+	if label != "api.openshift.com/addon-"+operatorId {
 		return false, nil
 	}
 
