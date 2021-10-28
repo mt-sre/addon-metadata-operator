28194943a2614a2073088244c3b99e55b1dc3ec5
ashish <asnaraya@redhat.com>
2021-10-28 14:14:07 +0530

    new interface types for bundle extractor and bundle parser
    Signed-off-by: ashish <asnaraya@redhat.com>

--- go.mod
+++ go.mod
@@ -3,12 +3,15 @@ module github.com/mt-sre/addon-metadata-operator
 go 1.16
 
 require (
+	github.com/Shopify/logrus-bugsnag v0.0.0-20171204204709-577dee27f20d // indirect
+	github.com/bshuster-repo/logrus-logstash-hook v1.0.2 // indirect
 	github.com/fatih/color v1.13.0
+	github.com/operator-framework/operator-registry v1.19.1
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.1.3
 	github.com/stretchr/testify v1.7.0
-	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
 	k8s.io/apimachinery v0.22.2
 	k8s.io/client-go v0.22.2
+	rsc.io/letsencrypt v0.0.3 // indirect
 	sigs.k8s.io/controller-runtime v0.10.1
 )
--- pkg/utils/types.go
+++ pkg/utils/types.go
@@ -2,17 +2,34 @@ package utils
 
 import (
 	"github.com/mt-sre/addon-metadata-operator/api/v1alpha1"
+	"github.com/operator-framework/operator-registry/pkg/registry"
 )
 
 type Validator struct {
-	Description string
-	Runner      ValidateFunc
+	Description        string
+	Runner             ValidateFunc
+	IsBundleValidation bool
+}
+
+type IndexImageExtractor interface {
+	ExtractBundlesFromImage(indexImage string, extractTo string) error
+	CacheKey(indexImage, addonName string) string
+	CacheHit(key string) bool
+	ExtractionPath() string
+	ManifestsPath(addonName string) string
+	CacheLocation() string
+	WriteToCache(value string) error
+}
+
+type BundleParser interface {
+	ParseBundles(addonName string, manifestsPath string) ([]registry.Bundle, error)
 }
 
 type ValidateFunc func(mb *MetaBundle) (bool, error)
 
 type MetaBundle struct {
 	AddonMeta *v1alpha1.AddonMetadataSpec
+	Bundles   []registry.Bundle
 	// TODO: add field for corresponding bundle
 }
 
@@ -21,4 +38,8 @@ func NewMetaBundle(addonMeta *v1alpha1.AddonMetadataSpec) *MetaBundle {
 	return &MetaBundle{
 		AddonMeta: addonMeta,
 	}
+}
+
+func (mb *MetaBundle) AddBundles(bundles []registry.Bundle) {
+	mb.Bundles = bundles
 }
