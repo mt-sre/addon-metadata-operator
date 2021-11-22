5218a1bc52cfcdea9b8c41097b1becd015ae5b72
ashish <asnaraya@redhat.com>
2021-11-22 20:22:42 +0530

    add integration tests for mtcli list-bundles cmd
    Signed-off-by: ashish <asnaraya@redhat.com>

--- pr_check.sh
+++ pr_check.sh
@@ -5,7 +5,7 @@ set -exvo pipefail -o nounset
 IMAGE_TEST=addon-metadata-operator
 
 docker build -t ${IMAGE_TEST} -f Dockerfile.ci .
-docker run --rm ${IMAGE_TEST} check test build
+docker run --rm ${IMAGE_TEST} check test test-e2e build
 
 # build addon-metadata-operator containers
 make docker-build
