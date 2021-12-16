0535d9dc277d1281268aa71bf582c47fa6feb02c
sblaisdo <sblaisdo@redhat.com>
2021-12-16 05:54:22 -0600

    Refactors.

--- pkg/validators/test_001_default_channel.go
+++ pkg/validators/test_001_default_channel.go
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package validators
 
 import (
@@ -59,3 +60,34 @@ func (v ValidatorTest001DefaultChannel) FailingCandidates() []utils.MetaBundle {
 		},
 	}
 }
+||||||| parent of e58bb06 (Refactors.)
+=======
+package validators
+
+import (
+	"github.com/mt-sre/addon-metadata-operator/internal/testutils"
+	"github.com/mt-sre/addon-metadata-operator/pkg/utils"
+)
+
+// check interface implemented
+var _ = utils.ValidatorTest(ValidatorTest001DefaultChannel{})
+
+type ValidatorTest001DefaultChannel struct{}
+
+func (v ValidatorTest001DefaultChannel) Name() string {
+	return "Addon Default Channel Validator"
+}
+
+func (v ValidatorTest001DefaultChannel) Run(mb utils.MetaBundle) (bool, string, error) {
+	return Validate001DefaultChannel(mb)
+}
+
+func (v ValidatorTest001DefaultChannel) SucceedingCandidates() []utils.MetaBundle {
+	return testutils.DefaultSucceedingCandidates()
+}
+
+// not implemented
+func (v ValidatorTest001DefaultChannel) FailingCandidates() []utils.MetaBundle {
+	return []utils.MetaBundle{}
+}
+>>>>>>> e58bb06 (Refactors.)
