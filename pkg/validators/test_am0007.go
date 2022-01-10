0c893536fb9298c2bfc57d70911a6af4bfd3615e
Yashvardhan Kukreja <ykukreja@redhat.com>
2022-01-10 17:43:20 +0530

    add: support for adding validator level assets and candidates for 0007 validator
    Signed-off-by: Yashvardhan Kukreja <ykukreja@redhat.com>

--- pkg/validators/test_am0007.go
+++ pkg/validators/test_am0007.go
@@ -1,8 +1,11 @@
 package validators
 
 import (
+	"github.com/mt-sre/addon-metadata-operator/api/v1alpha1"
 	"github.com/mt-sre/addon-metadata-operator/internal/testutils"
 	"github.com/mt-sre/addon-metadata-operator/pkg/types"
+	"github.com/mt-sre/addon-metadata-operator/pkg/utils"
+	"github.com/operator-framework/operator-registry/pkg/registry"
 )
 
 func init() {
@@ -19,11 +22,64 @@ func (v TestAM0007) Run(mb types.MetaBundle) types.ValidatorResult {
 	return AM0007.Runner(mb)
 }
 
-func (v TestAM0007) SucceedingCandidates() []types.MetaBundle {
-	return testutils.DefaultSucceedingCandidates()
+func (v TestAM0007) SucceedingCandidates() []utils.MetaBundle {
+	csvObj, err := yamlToDynamicObj("../validators/assets/am0007/csv.yaml") // from the pov of validate/validate_test.go
+	if err != nil {
+		panic(err)
+	}
+
+	succeedingCandidates := []utils.MetaBundle{
+		{
+			AddonMeta: &v1alpha1.AddonMetadataSpec{
+				InstallMode: "OwnNamespace",
+			},
+			Bundles: []registry.Bundle{
+				*registry.NewBundle("random-bundle", &registry.Annotations{}, &csvObj),
+			},
+		},
+		{
+			AddonMeta: &v1alpha1.AddonMetadataSpec{
+				InstallMode: "AllNamespaces",
+			},
+			Bundles: []registry.Bundle{
+				*registry.NewBundle("random-bundle", &registry.Annotations{}, &csvObj),
+			},
+		},
+	}
+	return append(succeedingCandidates, testutils.DefaultSucceedingCandidates()...)
 }
 
 // not implemented
-func (v TestAM0007) FailingCandidates() []types.MetaBundle {
-	return []types.MetaBundle{}
+func (v TestAM0007) FailingCandidates() []utils.MetaBundle {
+	csvObj, err := yamlToDynamicObj("../validators/assets/am0007/csv.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	return []utils.MetaBundle{
+		{
+			AddonMeta: &v1alpha1.AddonMetadataSpec{
+				InstallMode: "something",
+			},
+			Bundles: []registry.Bundle{
+				*registry.NewBundle("random-bundle", &registry.Annotations{}, &csvObj),
+			},
+		},
+		{
+			AddonMeta: &v1alpha1.AddonMetadataSpec{
+				InstallMode: "SingleNamespace",
+			},
+			Bundles: []registry.Bundle{
+				*registry.NewBundle("random-bundle", &registry.Annotations{}, &csvObj),
+			},
+		},
+		{
+			AddonMeta: &v1alpha1.AddonMetadataSpec{
+				InstallMode: "MultiNamespace",
+			},
+			Bundles: []registry.Bundle{
+				*registry.NewBundle("random-bundle", &registry.Annotations{}, &csvObj),
+			},
+		},
+	}
 }
