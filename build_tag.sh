edafb279c92053ad127763dee400057234c25331
Andrew Pantuso <apantuso@redhat.com>
2022-03-22 17:52:35 -0400

    fix: removing root from build_tag

--- build_tag.sh
+++ build_tag.sh
@@ -2,23 +2,4 @@
 
 set -exvo pipefail -o nounset
 
-# Run build_tag.sh in a sandbox to enforce golang & gcc bindings (CGO_ENABLED=1):
-# - sync secrets from app-interface:
-#   -> secrets: /resources/jenkins/global/secrets.yaml
-#   -> for job `gh-build-tag`: /resources/jenkins/global/templates.yaml
-#   -> doing cross-comiple builds
-#   -> local run: git tag v0.0.0 and ./build_tag.sh
-docker_run_args=(
-    --rm
-    --privileged
-    -e CGO_ENABLED=1
-    # github API token to post release
-    -e "GITHUB_TOKEN=${GITHUB_TOKEN}"
-    -v /var/run/docker.sock:/var/run/docker.sock
-    -v $(pwd):/go/src/github.com/mt-sre/addon-metadata-operator
-    -w /go/src/github.com/mt-sre/addon-metadata-operator
-    # goreleaser-cross version from https://github.com/goreleaser/goreleaser-cross/pkgs/container/goreleaser-cross
-    ghcr.io/goreleaser/goreleaser-cross:v1.17.6-v1.3.1
-    release --rm-dist
-)
-docker run "${docker_run_args[@]}"
+make release
