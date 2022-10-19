a91c3e7d992344b5f4b167dfc0288de63dd59704
Ankit152 <ankitkurmi152@gmail.com>
2022-10-19 14:25:11 +0530

    fix: [mtsre-741] AM0018 Addon pullSecretName should be a part of secrets
    Signed-off-by: Ankit152 <ankitkurmi152@gmail.com>

---
+++ pkg/validator/am0018/pull_secret_name_test.go
@@ -0,0 +1,102 @@
+package am0018
+
+import (
+	"testing"
+
+	"github.com/mt-sre/addon-metadata-operator/api/v1alpha1"
+	mtsrev1 "github.com/mt-sre/addon-metadata-operator/pkg/mtsre/v1"
+	"github.com/mt-sre/addon-metadata-operator/pkg/types"
+	"github.com/mt-sre/addon-metadata-operator/pkg/validator/testutils"
+)
+
+func TestNewPullSecretnameValid(t *testing.T) {
+	t.Parallel()
+
+	tester := testutils.NewValidatorTester(t, NewPullSecretname)
+	tester.TestValidBundles(map[string]types.MetaBundle{
+		"pullSecretName is not present and config is nil": {
+			AddonMeta: &v1alpha1.AddonMetadataSpec{
+				PullSecretName: "",
+				Config:         nil,
+			},
+		},
+		"pullSecretname is not present and secrets are nil": {
+			AddonMeta: &v1alpha1.AddonMetadataSpec{
+				PullSecretName: "",
+				Config: &mtsrev1.Config{
+					Secrets: nil,
+				},
+			},
+		},
+		"pullSecretName is present and addon has one secret": {
+			AddonMeta: &v1alpha1.AddonMetadataSpec{
+				PullSecretName: "test-pull-secret",
+				Config: &mtsrev1.Config{
+					Secrets: &[]mtsrev1.Secret{
+						{
+							Name: "test-pull-secret",
+						},
+					},
+				},
+			},
+		},
+		"pullSecretName is present and addon has multiple secrets defined": {
+			AddonMeta: &v1alpha1.AddonMetadataSpec{
+				PullSecretName: "test-pull-secret-2",
+				Config: &mtsrev1.Config{
+					Secrets: &[]mtsrev1.Secret{
+						{
+							Name: "test-pull-secret-1",
+						},
+						{
+							Name: "test-pull-secret-2",
+						},
+						{
+							Name: "test-pull-secret-2",
+						},
+					},
+				},
+			},
+		},
+	})
+}
+
+func TestNewPullSecretnameInvalid(t *testing.T) {
+	t.Parallel()
+
+	tester := testutils.NewValidatorTester(t, NewPullSecretname)
+	tester.TestInvalidBundles(map[string]types.MetaBundle{
+		"pullSecretName is not nil but addon Config is nil": {
+			AddonMeta: &v1alpha1.AddonMetadataSpec{
+				PullSecretName: "test-pull-secret",
+				Config:         nil,
+			},
+		},
+		"pullSecretName is not nil but addon secrets are nil": {
+			AddonMeta: &v1alpha1.AddonMetadataSpec{
+				PullSecretName: "test-pull-secret",
+				Config: &mtsrev1.Config{
+					Secrets: nil,
+				},
+			},
+		},
+		"pullSecretName is not nil but not present secret": {
+			AddonMeta: &v1alpha1.AddonMetadataSpec{
+				PullSecretName: "test-pull-secret",
+				Config: &mtsrev1.Config{
+					Secrets: &[]mtsrev1.Secret{
+						{
+							Name: "test-pull",
+						},
+						{
+							Name: "alpha",
+						},
+						{
+							Name: "beta",
+						},
+					},
+				},
+			},
+		},
+	})
+}
